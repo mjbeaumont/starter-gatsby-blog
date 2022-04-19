f10ec4a36af77e59b252e3d879edecb8160e58b2
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-19 19:52:49 -0400

    added bardavon link

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'gatsby';
 
 import * as styles from './social-icons.module.css';
@@ -13,7 +14,14 @@ export const SocialIcons = () => {
         <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
       </Link>
       <Link to="/">
-        <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+        <FontAwesomeIcon
+          icon={faInstagram}
+          className={styles.icon}
+          target="_blank"
+        />
+      </Link>
+      <Link to="https://www.bardavon.org/about/" target="_blank">
+        <FontAwesomeIcon icon={faGlobe} className={styles.icon} />
       </Link>
     </div>
   );
