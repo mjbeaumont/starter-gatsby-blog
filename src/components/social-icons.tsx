4858d8b57892f66ed8082f7bfb98eb625ee99902
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-18 22:02:00 -0400

    added facebook link

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -9,7 +9,7 @@ import * as styles from './social-icons.module.css';
 export const SocialIcons = () => {
   return (
     <div>
-      <Link to="/">
+      <Link to="https://www.facebook.com/MusiciansOfTheHVP/" target="_blank">
         <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
       </Link>
       <Link to="/">
