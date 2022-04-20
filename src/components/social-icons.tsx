6dab0d6bd2dbc0cd70aa9265d4dce2f5d3124e6a
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-19 22:14:10 -0400

    removed email from icons and added correct instagram link

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'gatsby';
 
 import * as styles from './social-icons.module.css';
@@ -13,15 +12,8 @@ export const SocialIcons = () => {
       <Link to="https://www.facebook.com/MusiciansOfTheHVP/" target="_blank">
         <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
       </Link>
-      <Link to="/">
-        <FontAwesomeIcon
-          icon={faInstagram}
-          className={styles.icon}
-          target="_blank"
-        />
-      </Link>
-      <Link to="https://www.bardavon.org/about/" target="_blank">
-        <FontAwesomeIcon icon={faGlobe} className={styles.icon} />
+      <Link to="https://www.instagram.com/musiciansofthehvp/" target="_blank">
+        <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
       </Link>
     </div>
   );
