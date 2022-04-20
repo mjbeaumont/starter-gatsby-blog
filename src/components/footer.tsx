172d3bf2875d8e6c8c86c48e22d02a6d5a5292cb
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-19 22:12:14 -0400

    added email to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,11 +3,20 @@ import React from 'react';
 import Container from './container';
 import * as styles from './footer.module.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      &copy; {new Date().getFullYear()} Musicians of the Hudson Valley
-      Philharmonic
+      <p>
+        <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+        <a href="mailto:info@hvpmusicians.org">info@hvpmusicians.org</a>
+      </p>
+      <p>
+        &copy; {new Date().getFullYear()} Musicians of the Hudson Valley
+        Philharmonic
+      </p>
     </div>
   </Container>
 );
