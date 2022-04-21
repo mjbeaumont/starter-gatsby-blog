8610ec382f8ffbc6b821ba2a11481fd92a7722c2
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-20 22:26:19 -0400

    removed hero image from article for now

--- src/pages/news.js
+++ src/pages/news.js
@@ -30,14 +30,6 @@ export const pageQuery = graphql`
         title
         slug
         publishDate(formatString: "MMMM Do, YYYY")
-        heroImage {
-          gatsbyImageData(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
         description {
           childMarkdownRemark {
             html
--- src/templates/news-article.js
+++ src/templates/news-article.js
@@ -83,12 +83,6 @@ export const pageQuery = graphql`
       author
       publishDate(formatString: "MMMM Do, YYYY")
       rawDate: publishDate
-      heroImage {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED, width: 1280)
-        resize(height: 630, width: 1200) {
-          src
-        }
-      }
       body {
         childMarkdownRemark {
           html
