b13c15f59712d4de20bc9272d68dd8c9ce7c1fc0
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-23 17:14:46 -0400

    created trackable link for GA

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -3,27 +3,29 @@ import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
+import { OutboundLink } from 'gatsby-plugin-google-gtag';
+
 import * as styles from './social-icons.module.css';
 
 export const SocialIcons = () => {
   return (
     <div>
-      <a
+      <OutboundLink
         href="https://www.facebook.com/MusiciansOfTheHVP/"
         target="_blank"
         rel="noreferrer"
       >
         <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
         <span className="visible-hidden">Facebook</span>
-      </a>
-      <a
+      </OutboundLink>
+      <OutboundLink
         href="https://www.instagram.com/musiciansofthehvp/"
         target="_blank"
         rel="noreferrer"
       >
         <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
         <span className="visible-hidden">Instagram</span>
-      </a>
+      </OutboundLink>
     </div>
   );
 };
