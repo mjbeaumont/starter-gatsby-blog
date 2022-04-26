e061d2ffd278ecc4ba8b106e07d030aca415af61
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-26 07:08:23 -0400

    added roster static data

---
+++ src/roster.ts
@@ -0,0 +1,141 @@
+export const sections = [
+  {
+    name: 'First Violin',
+    musicians: [
+      'Carole Cowan (Concertmaster)',
+      'Marka Young (Assistant Concertmaster)',
+      'Olga Dusheina',
+      'Irene Fitzgerald-Cherry',
+      'Rachel Handman',
+      'Linda Howard',
+      'Marla Rathbun',
+      'Michelle Stewart',
+      'Mitsuko Suzuki',
+      'Heather Vogel',
+    ],
+  },
+  {
+    name: 'Second Violin',
+    musicians: [
+      'Ji Min Lee (Principal)',
+      'Uli Speth (Assistant Principal)',
+      'RachelCrozier',
+      'Emily Frederick',
+      'Rhonni Hallman',
+      'Emily Kalish',
+      'Francia Mann',
+      'Sarah Tusch',
+    ],
+  },
+  {
+    name: 'Viola',
+    musicians: [
+      'Charlotte Malin (Principal)',
+      'Gregory Williams (Assistant Principal)',
+      'Elizabeth Handman',
+      'Isabella Mensz',
+      'Yumi Oshima',
+    ],
+  },
+  {
+    name: 'Cello',
+    musicians: [
+      'Susan Seligman (Principal)',
+      'Erica Pickhardt (Assistant Principal)',
+      'Aminda Asher',
+      'Joanne Choi',
+      'Madeline Fayette',
+      'Nanette Koc',
+    ],
+  },
+  {
+    name: 'Bass',
+    musicians: [
+      'Phillip Helm (Principal)',
+      'Richard Sosinsky (Assistant Principal)',
+      'Dan Merriman',
+      'Edith Stratton',
+    ],
+  },
+  {
+    name: 'Flute',
+    musicians: ['Marcia Gates (Principal)', 'Jill Sokol', 'Amy Hersh'],
+  },
+  {
+    name: 'Piccolo',
+    musicians: ['Amy Hersh'],
+  },
+  {
+    name: 'Oboe',
+    musicians: ['Joel Evans'],
+  },
+  {
+    name: 'English Horn',
+    musicians: ['Joel Evans'],
+  },
+  {
+    name: 'Clarinet',
+    musicians: ['Daniel Spitzer (Principal)', 'Adam Gallob', 'Larry Tietze'],
+  },
+  {
+    name: 'Bass Clarinet',
+    musicians: ['Larry Tietze'],
+  },
+  {
+    name: 'Bassoon',
+    musicians: ['Jeffrey Marchand (Principal)', 'Harvey Feldman'],
+  },
+  {
+    name: 'Horn',
+    musicians: [
+      'Nick Caluori (Principal)',
+      'Matthew Smith',
+      'Cody Halquist',
+      'Daniel Salera',
+    ],
+  },
+  {
+    name: 'Trumpet',
+    musicians: ['Terry Szor (Principal)', 'Sam Singeltary'],
+  },
+  {
+    name: 'Trombone',
+    musicians: ['Bradley Ward (Principal)', 'Paul Bellino'],
+  },
+  {
+    name: 'Bass Trombone',
+    musicians: ['Jack Noble'],
+  },
+  {
+    name: 'Tuba',
+    musicians: ['Ferran Martinez Miguel'],
+  },
+  {
+    name: 'Timpani',
+    musicians: ['Michael Singer (Principal)'],
+  },
+  {
+    name: 'Percussion',
+    musicians: [
+      'Kyle Ritenauer (Principal)',
+      'Matthew Beaumont',
+      'Aya Kaminaguchi',
+    ],
+  },
+  {
+    name: 'Keyboard',
+    musicians: ['Yalin Chi (Principal)'],
+  },
+  {
+    name: 'Harp',
+    musicians: ['Frances Duffy (Principal)'],
+  },
+  {
+    name: 'Orchestra Librarian',
+    musicians: ['Rhonni Hallman'],
+  },
+  {
+    name: 'Personnel Manager',
+    musicians: ['Marcia Gates'],
+  },
+];
