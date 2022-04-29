285fbed38c2117ac25a89467a0074a8f470191b3
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-04-28 20:22:02 -0400

    added a function to generate email body

---
+++ src/api/contact/generate-message-body.ts
@@ -0,0 +1,20 @@
+import { FormData } from './';
+
+export const generateMessageBody = ({
+  name,
+  email,
+  phone,
+  role,
+  message,
+}: FormData) => `
+<html> 
+<body>
+<p> Someone has filled out the contact form on hvpmusicians.org: </p>
+<p>
+  Name: ${name}<br />
+  Email: ${email}<br />
+  Phone: ${phone ?? 'Not Provided'}<br />
+  Role: ${role}<br />
+  Message: ${message}<br />
+</p>
+`;
