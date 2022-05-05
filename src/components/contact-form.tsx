75385456204ba91f186dab7e360bccda1a7cfdc6
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-05-04 21:22:23 -0400

    fixed cta

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -153,7 +153,9 @@ export const ContactForm = () => {
                   </label>
                 </div>
               </div>
-              <Cta disabled={isSubmitting}>Send Message</Cta>
+              <Cta disabled={isSubmitting} type="submit">
+                {isSubmitting ? 'Sending' : 'Send Message'}
+              </Cta>
             </Form>
           )}
         </Formik>
