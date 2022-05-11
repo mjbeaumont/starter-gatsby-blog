245b70f1bfc58a795d0c31590a5868c33ff15628
Matthew Beaumont <matthewbeaumont@Matthews-MacBook-Pro-2.local>
2022-05-10 20:15:03 -0400

    added renderer for asset links

--- src/components/short-history.tsx
+++ src/components/short-history.tsx
@@ -12,18 +12,35 @@ import {
   ContentfulRichTextGatsbyReference,
 } from 'gatsby-source-contentful/rich-text';
 
+import { INLINES } from '@contentful/rich-text-types';
+import type { RenderNode } from '@contentful/rich-text-react-renderer';
+
 import { OutboundLink } from 'gatsby-plugin-google-gtag';
 
 interface ShortHistoryProps {
   welcomeMessage: RenderRichTextData<ContentfulRichTextGatsbyReference>;
 }
 
+interface RenderNodeOptions {
+  renderNode: RenderNode;
+}
+
+const options: RenderNodeOptions = {
+  renderNode: {
+    [INLINES.ASSET_HYPERLINK]: (node, children) => {
+      const { uri } = node.data;
+      console.log({ node, children });
+      return <a href={uri}>{children}</a>;
+    },
+  },
+};
+
 export const ShortHistory = ({ welcomeMessage }: ShortHistoryProps) => {
   return (
     <section className={styles.container} id={HOME_MESSAGE_ID}>
       <Container>
         <SectionHeading className={styles.heading}>Welcome</SectionHeading>
-        {renderRichText(welcomeMessage)}
+        {renderRichText(welcomeMessage, options)}
         <p>
           <strong>Write to:</strong>
         </p>
