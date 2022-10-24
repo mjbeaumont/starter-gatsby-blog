a8d694958883b444db73615abec6d8a942bb458d
Matt Beaumont <mbeaumont@thefarmersdog.com>
2022-10-24 17:52:49 -0400

    added 2022/23 events

--- src/components/events.tsx
+++ src/components/events.tsx
@@ -11,15 +11,30 @@ export const Events = () => {
     <section className={styles.container} id={HOME_EVENTS_ID}>
       <Container>
         <SectionHeading className={styles.heading}>Events</SectionHeading>
-        <h2>Philharmonic Chamber Series</h2>
+        <h2>2022/2022 HVP Performances</h2>
+        <ul>
+          <li>
+            <strong>November 5th, 2022</strong>: Richard Einhorn’s “Voices of
+            Light”
+          </li>
+          <li>
+            <strong>December 14th, 2022</strong>: with “Mannheim Steamroller” at
+            the Poughkeepsie Civic Center
+          </li>
+          <li>
+            <strong>December 18th, 2022</strong>: Handel’s “Messiah”
+          </li>
+          <li>
+            <strong>May 20th, 2023</strong>: Holst’s “The Planets”
+          </li>
+        </ul>
+        <h2 className={styles.secondHeading}>Philharmonic Chamber Series</h2>
         <p>
-          We are thrilled to announce 3 chamber music events featuring musicians
+          We are thrilled to announce 2 chamber music events featuring musicians
           who play in the Hudson Valley Philharmonic. Admission is free, and
           there will be opportunities to meet and chat with the musicians after
-          the concerts. Bring your friends and family for sublime music on three
-          summer evenings!
+          the concerts. Bring your friends and family for sublime music!
         </p>
-
         <p>
           All events are at{' '}
           <a
@@ -31,35 +46,49 @@ export const Events = () => {
           </a>{' '}
           20 Carroll St, Poughkeepsie
         </p>
-
         <p>
-          <strong>July 17, 2022, 5pm</strong> – Music by Strauss, Beethoven,
-          Reinecke, Faure, and more.
+          <strong>Saturday, Nov. 12 @ 7:30pm</strong>
+        </p>{' '}
+        <p>
+          <em>String Quartet in D Major, op;. 64, “Lark”</em> - J. Haydn
+          <br />
+          Hudson Valley String Quartet: Marka Young, violin David Fiedler,
+          violin Lauren Byrne, viola Jeanne Fox, cello{' '}
         </p>
         <p>
-          Performed by Nick Calouri, horn; Yalin Chi, piano; Paul Bellino,
-          trombone; James Fitzwilliam, piano; Joel Evans, oboe; Ruthanne
-          Schempf, piano
+          <em>Cello Suite No. 2 in D Minor, BWV 1008</em> - JS Bach
+          <div className={styles.movements}>
+            Prelude
+            <br />
+            Allemande
+            <br />
+            Courante
+            <br /> Sarabande
+            <br /> Menuet I & II
+            <br /> Gigue
+            <br />
+          </div>{' '}
+          Christopher Santos, cello
         </p>
-
         <p>
-          <strong>July 31, 2022, 5pm</strong> – Music by Bach, Coleridge-Taylor,
-          Bartok, Mozart, Grandjany, Andres, and Lutoslawski.
+          <em>Five Duos for Two Cellos</em> - Halsey Stevens <br />
+          Nanette Koch, Susan Seligman, cello cello
+        </p>
+        <p>
+          <em>String Quartet in F Major, Op. 96 No. 12 (“American”)</em> -
+          Dvorak
+          <br />
+          Hudson Valley String Quartet
+        </p>
+        <p>
+          <strong>August 14, 2022 @ 5pm</strong> – Music by Jesse Montgomery,
+          Prokofiev, and more
           <br />
         </p>
         <p>
-          Performed by Susan Seligman, cello; Emily Kalish, violin; Kyle Walker,
-          piano; Heather Vogel and Rachel Crozier, violins; Marcia Gates, flute;
-          Frances Duffy, harp
-        </p>
-        <p>
-          <strong>August 14, 2022, 5pm</strong> – Music for a Summer Evening
-          <br />
-        </p>
-        <p>
-          Performed by Rachel Handman, Heather Vogel, and Francie Mann, violins;
-          Greg Williams, viola; Nanette Koch, cello; and James Fitzwilliam,
-          piano
+          Performed by The Handman String Quartet; Christopher Santos, cello;
+          Amy Hersh, flute, Kathleen Thomson, violin, Katherine Sinabaugh,
+          viola, Jacqueline Stern, cello
         </p>
       </Container>
     </section>
